9bfcb7a486495ef2e7fa81cc78671aec30496af2
José Ismael De los santos <ismaeldg01@gmail.com>
2026-06-21 17:34:13 -0400

    Corrigiendo rutas de las paginas

--- tests/CorrectLogin.spec.js
+++ tests/CorrectLogin.spec.js
@@ -1,10 +1,10 @@
 const { test, expect } = require("@playwright/test");
 
-const { BasePage } = require("../pages/BasePage.js");
-const { HomePage } = require("../pages/HomePage.js");
-const { LoginPage } = require("../pages/LoginPage.js");
+const { BasePage } = require("./pages/BasePage.js");
+const { HomePage } = require("./pages/HomePage.js");
+const { LoginPage } = require("./pages/LoginPage.js");
 const testData = JSON.parse(
-  JSON.stringify(require("../testData/credentials.json")),
+  JSON.stringify(require("./testData/credentials.json")),
 );
 
 test("Correct Login", async ({ page }) => {
