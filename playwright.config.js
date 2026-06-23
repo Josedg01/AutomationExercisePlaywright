0a7ee2a47e2606534289ee9b0054f0296ed9a8d2
José Ismael De los santos <ismaeldg01@gmail.com>
2026-06-23 01:08:09 -0400

    Configuring github pages with allure report

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ["html", { open: "on-failure" }],
+    ["html", { outputFile: "playwright-report", open: "never" }],
     ["junit", { outputFile: "test-results/results.xml" }],
     ["allure-playwright"],
   ],
